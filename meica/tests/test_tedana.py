cdad63e048cfa055d70d9f3f1089a9f3efe0a365
Elizabeth DuPre <emd222@cornell.edu>
2017-10-29 23:14:37 -0400

    [BUG] Update outdir location in testing

--- meica/tests/test_tedana.py
+++ meica/tests/test_tedana.py
@@ -14,4 +14,4 @@ def test_basic_tedana():
     options = parser.parse_args(['-d', '/home/neuro/data/zcat_ffd.nii.gz',
                                  '-e', '14.5', '38.5', '62.5'])
     tedana.main(options)
-    assert os.path.isfile('TED/comp_table.txt')
+    assert os.path.isfile('comp_table.txt')
